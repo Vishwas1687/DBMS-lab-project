85d1995a43a7ca49f67e6620184f48dc0edd13b0
Vishwas1687 <gpvishwas1687@gmail.com>
2023-04-26 14:15:59 +0530

    small-change

--- api/routes/categoryRoutes.js
+++ api/routes/categoryRoutes.js
@@ -1,3 +1,18 @@
+const express=require('express')
+const { model } = require('mongoose')
+const router=express.Router()
+
+// router.post('/create-category',createCategoryController)
+
+// router.put('/update-category',updateCategoryController)
+
+// router.delete('/delete-category',deleteCategoryController)
+
+// router.get('/get-category/:slug',getSingleCategoryController)
+
+
+// router.delete('/delete-category',deleteCategoryController)
 
 // router.get('/get-category/:slug',getSingleCategoryController)
 module.exports=router
+
