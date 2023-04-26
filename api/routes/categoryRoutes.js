59bbb2239bd75d45ff540e18ec35afb152f631bb
Vishwas1687 <gpvishwas1687@gmail.com>
2023-04-26 14:10:28 +0530

    product-category-Schema

---
+++ api/routes/categoryRoutes.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const { model } = require('mongoose')
+const router=express.Router()
+
+// router.post('/create-category',createCategoryController)
+
+// router.put('/update-category',updateCategoryController)
+//
+// router.delete('/delete-category',deleteCategoryController)
+
+// router.get('/get-category/:slug',getSingleCategoryController)
+module.exports=router 
