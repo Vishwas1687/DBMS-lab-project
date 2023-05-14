7e0857f9e5cbc4d23bab407a5d27501c0262a2a9
Aditya Tyagi <zadityagiz@gmail.com>
2023-05-14 14:03:43 +0530

    Improved UI for product pages and added related products.

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -10,11 +10,22 @@ const ProductPage = () => {
   const { slug } = useParams();
   const [info, setInfo] = useState({})
   const [found,setFound] = useState(false);
+  const [selectedWeight, setSelectedWeight] = useState(0);
+  const [relatedProducts,setRelatedProducts] = useState([]);
+  const [sameBrand,setSameBrand] = useState([]);
+
 
   useEffect(() => {
     const fetchData = async () => {
         try{
             const {data} = await axios.get(`http://localhost:5000/api/products/get-single-product/${slug}`);
+            const brand = data.existingProduct.brand._id;
+            const subcat = data.existingProduct.subcategory;
+            const temp = await axios.get(`http://localhost:5000/api/products/get-related-products-of-the-subcategory/${slug}/`);
+            const tempBrand = await axios.get(`http://localhost:5000/api/products/get-products-based-on-brand-and-subcategory-other-than-current-product/${brand}/${subcat}/${slug}`);
+
+            setRelatedProducts(temp.data.products);
+            setSameBrand(tempBrand.data.products);
             if (!data.success){
                 setFound(false);
                 setInfo({});
@@ -30,7 +41,9 @@ const ProductPage = () => {
     fetchData();
   }, [slug]);
 
-
+  useEffect(() => {
+  }, [relatedProducts,sameBrand])
+  
   return (
     <>
     {found ? <>
@@ -53,10 +66,73 @@ const ProductPage = () => {
         </div>
         <div className='product'>
             <img className="productImage" src = {`http://localhost:5000/api/products/get-photo/${info.slug}`} alt='Product Image' />
-            <div className='productName_'>{info.product_name}</div>
+            <div className='details'>
+                <div className='brandName_'>{info.brand.brand_name}</div>
+                <div className='productName_'>{info.product_name}</div>
+                <div className='catSub'><span className='product_cat'>{info.category.category_name}</span>•<span className='product_subcat'>{info.subcategory}</span></div>
+                <div className="mrp_">MRP: <s>Rs.{info.weights[selectedWeight].mrp}</s></div>
+                <div className="sp_">PRICE: Rs.{info.weights[selectedWeight].sp}</div>
+                <div className='weightS'>
+                    <div>
+                        Select quantity:
+                    </div>
+                    <select onChange={e => setSelectedWeight(e.target.value)}>
+                        {info ? info.weights.map((weight,index)=>{
+                            return <>
+                                <option value={index} key={index}>{weight.weight} {weight.weight_units}</option>
+                            </>
+                        }) : ""}
+                    </select>
+                </div>
+                <div className='addTC'>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
         </div>
     </div>
-    
+                        <hr></hr>
+
+    <div className="related">
+        <h3>Related Products</h3>
+        <div className="productSlider">
+            {
+                (relatedProducts.length) ? 
+                relatedProducts.map((product,index)=>(
+                        <div className="card__" key={index}>
+  <img src={`http://localhost:5000/api/products/get-photo/${product.slug}`} className="card-img_" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title" style={{textTransform:'capitalize'}}>{product.product_name}</h5>
+    <a href={`/product/${product.slug}`} className="btn btn-primary">Buy</a>
+  </div>
+</div>
+                ))
+                 : <>No products found...</>
+            }
+        
+        </div>
+    </div>
+
+    <hr></hr>
+
+    <div className="related brand">
+        <h3>Similar products by {info.brand.brand_name}</h3>
+        <div className="productSlider">
+            {
+                (sameBrand.length) ? 
+                sameBrand.map((product,index)=>(
+                        <div className="card__" key={index}>
+  <img src={`http://localhost:5000/api/products/get-photo/${product.slug}`} className="card-img_" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title" style={{textTransform:'capitalize'}}>{product.product_name}</h5>
+    <a href={`/product/${product.slug}`} className="btn btn-primary">Buy</a>
+  </div>
+</div>
+                ))
+                 : <>No products found...</>
+            }
+        
+        </div>
+    </div>
 
     
     </> : <PageNotFound />}
