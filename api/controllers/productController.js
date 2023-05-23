1f632470af83af7e634906ebfee9b266f18a7c61
Vishwas1687 <gpvishwas1687@gmail.com>
2023-05-23 12:33:30 +0530

    orders-delete-should-be-checked

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,7 +1,7 @@
 const { response } = require('express')
 const slugify=require('slugify')
 const mongoose=require('mongoose')
-// const OrderModel=require('../models/Order')
+const OrderModel=require('../models/Order')
 const CategoryModel=require('../models/Category')
 const fs=require('fs')
 const ProductModel=require('../models/Product')
@@ -193,6 +193,7 @@ const deleteProductController=async(req,res)=>{
     // );
 
        await ProductModel.findByIdAndDelete(existingProduct._id)
+       await OrderModel.deleteMany({"items.product":existingProduct._id})
        res.send({
            message:`Product ${slug} deleted successfully`,
            success:true
