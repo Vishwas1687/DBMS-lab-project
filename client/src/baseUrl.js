818d6d97a850c65d4622a01871f17c30d3621e94
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2023-09-27 14:12:28 +0530

    new build

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl='http://localhost:5000'
-// export const baseUrl='https://ecommerce-grocery.cyclic.app'
+// export const baseUrl='http://localhost:5000'
+export const baseUrl='https://ecommerce-grocery.cyclic.app'
